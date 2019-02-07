45e8723397b4533643c322a76099698f611b3809
tumtpk <pk.prateep@gmail.com>
2019-02-07 11:50:58 +0700

    init project

---
+++ application/tests/models/Example_model_test.php
@@ -0,0 +1,22 @@
+<?php 
+
+class Example_model_test extends TestCase
+{
+    public function setUp()
+    {
+        $this->resetInstance();
+        $this->CI->load->model('Test_model');
+        $this->obj = $this->CI->Test_model;
+    }
+
+    public function test_get_test_list()
+    {
+        $expected = "hello";
+        $list = $this->obj->test($expected);
+        $this->assertEquals($expected, "asdasd");
+    }
+
+
+}
+
+?>
